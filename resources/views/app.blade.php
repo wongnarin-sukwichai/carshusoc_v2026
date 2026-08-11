0cdab4a4d516cf662403bcfe623c17da466cbf4d
wongnarin <wongnarin.s@msu.ac.th>
2026-08-11 10:00:36 +0700

    update

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,7 +4,9 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title inertia>{{ config('app.name', 'Laravel') }}</title>
+        <title inertia>{{ config('app.name', 'CARS-HUSOC MSU') }}</title>
+
+        <link rel="icon" type="image/png" href="/images/logo-cars-husoc.png">
 
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=anuphan:400,500,600,700" rel="stylesheet" />
